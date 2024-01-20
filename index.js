48f22576494c2cbc2eb52be82d3fa4d869be77b2
Swastik07822 <116582848+Swastik07822@users.noreply.github.com>
2024-01-20 15:50:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function generateRandomNumber(){
+
+    let playerOne = Math.floor(Math.random()*(5) + 1);
+
+    let playerTwo = Math.floor(Math.random()*(5) + 1);
+
+    console.log(playerOne,playerTwo);
+
+
+    let imgOne = "dice" + playerOne.toString() + ".png";
+
+    let imgTwo = "dice" + playerTwo.toString() + ".png";
+
+    document.getElementById("dice-one").setAttribute("src",imgOne);
+    document.getElementById("dice-two").setAttribute("src",imgTwo);
+    
+    let winner = "";
+
+    if (playerOne > playerTwo){
+
+        winner = "Player-1 wins the game";
+    }
+
+    else if(playerOne < playerTwo){
+
+        winner = "Player-2 wins the game";
+    }
+
+    else{
+
+        winner = "Draw";
+    }
+
+    document.getElementById("winner").innerText = winner;
+
+}
+
+
+
